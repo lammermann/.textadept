828500dde338f7b108e80994806e3ba412525cff
Benjamin Kober <kober@optisense.com>
2013-11-20 21:06:34 +0100

    added upstream projekts.

--- init.lua
+++ init.lua
@@ -2,4 +2,6 @@ require 'textadept'
 require 'textredux.hijack'
 vim = require 'vim'
 
+if not CURSES then ui.set_theme('base16-solarized-light') end
+
 keys = vim.use_vim_modes(keys)
---
+++ pull_upstream.sh
@@ -0,0 +1,2 @@
+git subtree pull --prefix modules/markdown https://github.com/rgieseke/ta-markdown.git master
+git subtree pull --prefix modules/themes https://github.com/rgieseke/ta-themes.git master
